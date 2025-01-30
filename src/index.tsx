d70083eba5f61ef36808caf69d3107631f6765c2
2pasha <kostishinpavlo24@gmail.com>
2025-01-30 13:25:36 +0200

    fix error with deployment when changing router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter as HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './store';
 import 'bulma/css/bulma.min.css';
 
